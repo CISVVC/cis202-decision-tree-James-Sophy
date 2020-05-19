c25a15bc4404a5da5f3e512beb50c070a8993fa2
James-Sophy <gunz13580@hotmail.com>
2020-05-18 19:28:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_tree.h"
 
 Binary_tree read(istream& in)
@@ -30,6 +32,20 @@ Binary_tree read(istream& in)
 //      Note:  it is a recursive call
 //
 //    return Binary_tree(data, left, right)
+	std::string data;
+	int level;
+	std::getline(in, data);
+	level = atoi(&data[0]);
+	const char * data_str = data.c_str();
+	while(!isalpha(*data_str) && data.length() > 2){
+		data_str++;
+	}
+	data = data_str;
+	if(level == -1) return Binary_tree();
+	Binary_tree left = read(in);
+	Binary_tree right = read(in);
+	
+	return Binary_tree(data, left, right);
 }
 
 void write(ostream& out, const Binary_tree& tree, int level)
@@ -40,6 +56,13 @@ void write(ostream& out, const Binary_tree& tree, int level)
 //    output level + ' ' + tree.data() + \n to out
 //    call write (out, tree.left(), level + 1)
 //    call write (out, tree.right(), level + 1)
+	if(tree.empty()){
+		out << "-1\n";
+		return;
+	}
+	out << level << " " << tree.data() << "\n";
+	write(out, tree.left(), level + 1);
+	write(out, tree.right(), level + 1);
 }
 
 /*
@@ -125,7 +148,49 @@ int main(int argc,char **argv)
     // When done, write the decision tree to the data file by calling
     // the write function so that the new
     // question and answer is added to the data file
+    string file = "questions.dat";
+    std::fstream in(file);
+    root = read(in);
+    Binary_tree question_tree = root;
+    bool done = false;
+    while (!done){
+    	std::string response;
+    	Binary_tree left = question_tree.left();
+    	Binary_tree right = question_tree.right();
+    	if(left.empty() && right.empty()){
+    		do {
+    			std::cout << "Is it a " << question_tree.data() << "? (y/n): ", std::cin >> response;
+    		} while (response != "y" && response != "n");
+    		if(response == "y"){
+    			std::cout << "I guessed it!\n";
+    		} else {
+    			string question;
+    			std::cout << "I give up. What is it?: ", std::cin >> response;
+    			std::cout << "Please give me a question that is true for a " << response << " and false for a " << question_tree.data() << "\n";
+    			do {
+    				std::getline(std::cin, question);
+    			} while(question.length() == 0);
+    			question_tree.set(question, Binary_tree(response), Binary_tree(question_tree.data()));
+    		}
+    		do {
+    			std::cout << "May I try again? (y/n): ", std::cin >> response;
+    		} while(response != "y" && response != "n");
+    		if(response == "y"){
+    			question_tree = root;
+    		} else {
+    			done = true;
+    		}
+    	} else {
+    		do {
+    			std::cout << question_tree.data() << "(y/n): ", std::cin >> response;
+    		} while (response != "y" && response != "n");
+    		if(response == "y"){
+    			question_tree = left;
+    		} else {
+    			question_tree = right;
+    		}
+    	}
+    }
+    //write function refuses to work and I can't figure out why
+    //write(in, root, 0);
 }
-
-
-
